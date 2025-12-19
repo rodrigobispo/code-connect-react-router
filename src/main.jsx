e13c99cd28d3c9eebb632f2cb33290f94d62ea19
Rodrigo Augusto Carlos Bispo <rodrigo.bispo@infrasa.gov.br>
2025-12-18 21:10:34 -0300

    chore: organiza rotas.

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,10 @@
 import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Login } from './pages/Login/index.jsx'
-import { Register } from './pages/Register/index.jsx'
-import { Feed } from './pages/Feed/index.jsx'
-import { BlogPost } from './pages/BlogPost/index.jsx'
-import { BrowserRouter, Route, Routes } from 'react-router'
-import { ProtectedRoute } from './components/ProtectedRoute'
-import { Logout } from './pages/Logout'
+import { AppRouter } from './router/AppRouter/index.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/auth'>
-          <Route path='login' element={<Login />} />
-          <Route path='logout' element={<Logout />} />
-          <Route path='register' element={<Register />} />
-        </Route>
-        <Route path='/'>
-          <Route path='' element={
-            <ProtectedRoute>
-              <Feed />
-            </ProtectedRoute>
-          } />
-          <Route path='blog-post/:slug' element={
-            <ProtectedRoute>
-              <BlogPost />
-            </ProtectedRoute>
-          } />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <AppRouter />
   </StrictMode>,
 )
---
+++ src/router/AppRouter/index.jsx
@@ -0,0 +1,33 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import { Login } from "../../pages/Login";
+import { Logout } from "../../pages/Logout";
+import { Register } from "../../pages/Register";
+import { ProtectedRoute } from "../../components/ProtectedRoute";
+import { BlogPost } from "../../pages/BlogPost";
+import { Feed } from '../../pages/Feed';
+
+export function AppRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/auth'>
+          <Route path='login' element={<Login />} />
+          <Route path='logout' element={<Logout />} />
+          <Route path='register' element={<Register />} />
+        </Route>
+        <Route path='/'>
+          <Route path='' element={
+            <ProtectedRoute>
+              <Feed />
+            </ProtectedRoute>
+          } />
+          <Route path='blog-post/:slug' element={
+            <ProtectedRoute>
+              <BlogPost />
+            </ProtectedRoute>
+          } />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
