ba5636e2f02bbfa8212c7b56ab8ed93cb252d35b
Rodrigo Augusto Carlos Bispo <rodrigo.bispo@infrasa.gov.br>
2025-12-23 14:12:38 -0300

    feat: implementa like.

--- src/components/CardPost/ThumbsUpButton.jsx
+++ src/components/CardPost/ThumbsUpButton.jsx
@@ -2,9 +2,9 @@ import { IconButton } from "../IconButton"
 import { Spinner } from "../Spinner"
 import { IconThumbsUp } from "../icons/IconThumbsUp"
 
-export const ThumbsUpButton = ({ loading }) => {
+export const ThumbsUpButton = ({ loading, ...props }) => {
     return (
-        <IconButton disabled={loading}>
+        <IconButton disabled={loading} {...props}>
             { loading ? <Spinner /> : <IconThumbsUp /> }
         </IconButton>
     )
--- src/components/CardPost/index.jsx
+++ src/components/CardPost/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { Author } from "../Author"
 import { ModalComment } from "../ModalComment"
 import styles from './cardpost.module.css'
@@ -6,38 +7,54 @@ import { ThumbsUpButton } from "./ThumbsUpButton"
 import { Link } from "react-router"
 
 export const CardPost = ({ post }) => {
-    return (
-        <article className={styles.card}>
-            <header className={styles.header}>
-                <figure className={styles.figure}>
-                    <img
-                        src={post.cover}
-                        alt={`Capa do post de titulo: ${post.title}`}
-                    />
-                </figure>
-            </header>
-            <section className={styles.body}>
-                <h2>{post.title}</h2>
-                <p>{post.body}</p>
-                <Link to={`/blog-post/${post.slug}`}>Ver detalhes</Link>
-            </section>
-            <footer className={styles.footer}>
-                <div className={styles.actions}>
-                    <div className={styles.action}>
-                        <ThumbsUpButton loading={false} />
-                        <p>
-                            {post.likes}
-                        </p>
-                    </div>
-                    <div className={styles.action}>
-                        <ModalComment />
-                        <p>
-                            {post.comments.length}
-                        </p>
-                    </div>
-                </div>
-                <Author author={post.author} />
-            </footer>
-        </article>
-    )
+
+  const [likes, setLikes] = useState(post.likes);
+
+
+  const handleLikeButton = () => {
+    fetch(`http://localhost:3000/blog-posts/${post.id}/like`, {
+      method: 'post'
+    })
+      .then(response => {
+        if (response.ok) {
+          setLikes(oldState => oldState + 1);
+          console.log('incrementa like');
+        }
+      })
+  }
+
+  return (
+    <article className={styles.card}>
+      <header className={styles.header}>
+        <figure className={styles.figure}>
+          <img
+            src={post.cover}
+            alt={`Capa do post de titulo: ${post.title}`}
+          />
+        </figure>
+      </header>
+      <section className={styles.body}>
+        <h2>{post.title}</h2>
+        <p>{post.body}</p>
+        <Link to={`/blog-post/${post.slug}`}>Ver detalhes</Link>
+      </section>
+      <footer className={styles.footer}>
+        <div className={styles.actions}>
+          <div className={styles.action}>
+            <ThumbsUpButton loading={false} onClick={handleLikeButton} />
+            <p>
+              {likes}
+            </p>
+          </div>
+          <div className={styles.action}>
+            <ModalComment />
+            <p>
+              {post.comments.length}
+            </p>
+          </div>
+        </div>
+        <Author author={post.author} />
+      </footer>
+    </article>
+  )
 }
