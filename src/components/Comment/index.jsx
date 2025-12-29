0a5c6c0aafa21f980f7cda56d290687ce3b9a5cf
Rodrigo Augusto Carlos Bispo <rodrigo.bispo@infrasa.gov.br>
2025-12-29 17:30:46 -0300

    feat: exbibe icone de comentario somente ao usuario que postou o comentario.

--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -1,12 +1,19 @@
 import styles from './comment.module.css'
 import { Avatar } from "../Avatar"
+import { ModalComment } from '../ModalComment/index';
+import { useAuth } from '../../hooks/useAuth';
 
 export const Comment = ({ comment }) => {
+
+  const { user } = useAuth()
+
+  const isOwner = user && (user.id == comment.author.id)
 
   return (<div className={styles.comment}>
     <Avatar author={comment.author} />
     <strong>@{comment.author.name}</strong>
     <p>{comment.text}</p>
     <div className={styles.divider} />
+    {isOwner && <ModalComment isEditing={isOwner} />}
   </div>)
 }
